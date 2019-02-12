df3d0cf6238b6d4006654ae0bc3693356e938de7
Magdy Elmansy <magdyelmansy4@gmail.com>
2019-02-12 14:58:38 +0200

    Add delete route And Testing it

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID is Invalide');
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send('Todo Not Deleted');
+    }
+    return res.status(200).send({todo})
+  }, (e) => {
+    res.status(400).send();
+  })
+});
+
 app.post('/users', (req, res) => {
   var user = new User({
     email: 'magdyelmansy@gmail.com'
