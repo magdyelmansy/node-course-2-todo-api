d869f2215a9847641388581cab3d8ab4917d1766
Magdy Elmansy <magdyelmansy4@gmail.com>
2019-02-14 15:50:39 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -59,6 +60,28 @@ app.delete('/todos/:id', (req, res) => {
   }, (e) => {
     res.status(400).send();
   })
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID is Invalide');
+  }
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+    body.completed = false;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send('Todo Not Updated');
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.stats(400).send();
+  });
 });
 
 app.post('/users', (req, res) => {
